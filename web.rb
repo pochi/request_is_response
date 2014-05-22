28996e48a7e783d6ee9fb41be4d4b49102327717
pochi <pochi.black@gmail.com>
2014-05-22 19:59:18 +0900

    change view

--- web.rb
+++ web.rb
@@ -3,14 +3,28 @@ require 'sinatra'
 set :public_dir, File.dirname(__FILE__) + "/public"
 set :views, File.dirname(__FILE__) + "/view"
 
+=begin
 use Rack::Auth::Basic do |username, password|
   username == "request" && password == "response"
 end
+=end
 
 
 helpers do
   def request_headers
     env.inject({}){|acc, (k,v)| acc[$1.downcase] = v if k =~ /^http_(.*)/i; acc}
+  end
+
+  def authenticate!
+    unless authorized?
+      response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+      throw(:halt, [401, "Not authorized¥n"])
+    end
+  end
+
+  def authorized?
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credendials == ['request', 'response']
   end
 end
 
@@ -21,6 +35,7 @@ end
 get '/' do
   # request.inspect
   puts "------------"
+  authenticate!
   @request_headers = request_headers.merge(params)
   haml :index
 end
