bb2020aff65b0e6bdb32925f21f8addf5728699b
pochi <pochi.black@gmail.com>
2015-05-29 16:59:08 +0900

    modified request

--- examples/too_long_request.rb
+++ examples/too_long_request.rb
@@ -6,5 +6,10 @@ http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 post = Net::HTTP::Post.new("/too_long_request")
 puts "begin request " + Time.now.inspect
+
+puts http.get("/too_long_request").inspect
+puts "end request " + Time.now.inspect
+
+puts "begin request " + Time.now.inspect
 puts http.start {|h| h.request(post) }.inspect
 puts "end request " + Time.now.inspect
